77b75a7736d6b19223c30181eb73298742c3b12d
Agustin Cao <alcaolpg@gmail.com>
2021-06-11 18:55:48 -0300

    Base proyecto AS7

---
+++ codigo/generador/generador/main.c
@@ -0,0 +1,18 @@
+/*
+ * generador.c
+ *
+ * Created: 11/06/2021 18:54:46
+ * Author : Agustín
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
