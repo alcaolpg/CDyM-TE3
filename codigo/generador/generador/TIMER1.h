3bec560094253e42a61a14251c6e90ba4311400f
soyezequiel <alcayaga147147@alu.ing.unlp.edu.ar>
2021-06-18 12:25:59 -0300

    correcciones ortográficas

--- codigo/generador/generador/TIMER1.h
+++ codigo/generador/generador/TIMER1.h
@@ -10,12 +10,12 @@
 #define F_max 10000
 
 /*******************************************************************************************************************
-Este "objeto" permite administrar una onda cuadrada, generada por el TIMER1, esta onda es tiene una salida por el 
-bit1 del puerto B, tambien llamado OC1A, del atmega328P 
+Este "objeto" permite administrar una onda cuadrada, generada por el TIMER1,
+esta onda es tiene una salida por el bit1 del puerto B, también llamado OC1A, del atmega328P
 *******************************************************************************************************************/
 
-//funciones publicas
-void TIMER1_set_module(); //Configuracion inicial del Timer1
+//funciones públicas
+void TIMER1_set_module(); //Configuración inicial del Timer1
 uint8_t TIMER1_set_frequency(char frequencyStr[] );// recibe la frecuencia por string que tendra la onda generada por el timer1, retorna 1 si esta entre 100 y 10000, 0 en caso contrario
 void TIMER1_set_on(); //Al invocarse la función, comienza a producir un sonido.
 void TIMER1_set_off(); //Al invocarse la función, parar de reproducir un sonido.
