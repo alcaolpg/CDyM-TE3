3ca8352ed6d72e653b10056b8860bac5d346c5f8
Agustin Cao <alcaolpg@gmail.com>
2021-06-20 20:18:30 -0300

    Implementación buffer circular

--- codigo/generador/generador/uart_rx.c
+++ codigo/generador/generador/uart_rx.c
@@ -13,10 +13,10 @@
 void RX_enable(void);
 void RX_interrupt_enable(void);
 void set_uart_rx(unsigned char *buff_adress, int buff_size);
-void recive_data(void);
+void recive_data(char * RX_Data);
 void ready_to_recive(void);
 void done_reciving(void);
-void recive_buffer_content(char);
+void recive_buffer_content(void);
 
 unsigned char *buffer;
 unsigned char index; //puntero de lectura para el buffer
@@ -48,20 +48,23 @@ void set_uart_rx(unsigned char *buff_adress, int buff_size){
 	RX_interrupt_enable();
 }
 
-char recive_buffer_content(char * c)
+char recive_buffer_content(void)
 {
-	volatile char RX_Data = 0;
+	volatile char RX_Data;
 
 	recive_data(RX_Data);
-	if(RX_Data!='\r' && index < size_buff){
+	if(RX_Data!='\r'){
+		if (index == size_buff)
+		{
+			index = 0;
+		}
 		buffer[index++]=RX_Data;
 	}
 	else{
 		//	    BufferRX[Index++]='\r'; //opcional
 		//		BufferRX[Index++]='\n';
-		buffer[index]='\0';
-		index=0;
-		done_reciving();
+		buffer[index++]='\0';
+		//done_reciving();
 	}
 }
 
