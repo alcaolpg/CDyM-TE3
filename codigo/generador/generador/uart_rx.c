61e8a09b8ec21de015fd60b8da71afe1fd1362eb
Markos <marmoscoso1@gmail.com>
2021-06-20 19:30:16 -0300

    cosas del modulo de recepción
    cosas de recepción

--- codigo/generador/generador/uart_rx.c
+++ codigo/generador/generador/uart_rx.c
@@ -12,7 +12,7 @@
 
 void RX_enable(void);
 void RX_interrupt_enable(void);
-void set_uart_rx(unsigned char *buff_adress);
+void set_uart_rx(unsigned char *buff_adress, int buff_size);
 void recive_data(void);
 void ready_to_recive(void);
 void done_reciving(void);
@@ -20,6 +20,8 @@ void recive_buffer_content(char);
 
 unsigned char *buffer;
 unsigned char index; //puntero de lectura para el buffer
+unsigned char size_buff;
+unsigned char FLAGRECIVE;
 
 void RX_enable(void)
 {
@@ -31,21 +33,17 @@ void RX_interrupt_enable(void)
 	UCSR0B |= (1<<UDRIE0); //ver esto
 }
 
-void ready_to_recive(void)
-{
-	UCSR0B |= (1<<RXCIE0);
-}
-
 void done_reciving(void)
 {
-	UCSR0B &=~(1<<RXCIE0);
+	FLAGRECIVE = 1;
 }
 void recive_data(char * RX_Data){
 	RX_Data = UDR0;
 }
-void set_uart_rx(unsigned char *buff_adress){
-	buffer = buff_adress;}
+void set_uart_rx(unsigned char *buff_adress, int buff_size){
+	buffer = buff_adress;
 	index = 0;
+	size_buff = buff_size;
 	RX_enable();
 	RX_interrupt_enable();
 }
@@ -55,7 +53,7 @@ char recive_buffer_content(char * c)
 	volatile char RX_Data = 0;
 
 	recive_data(RX_Data);
-	if(RX_Data!='\r'){
+	if(RX_Data!='\r' && index < size_buff){
 		buffer[index++]=RX_Data;
 	}
 	else{
